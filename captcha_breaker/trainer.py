84f9463c6fbd5c4f6c0c9d248cf90968542b23a8
DropletProbe <ys15162@my.bristol.ac.uk>
2018-06-27 11:55:58 +0800

    nothing

--- captcha_breaker/trainer.py
+++ captcha_breaker/trainer.py
@@ -12,6 +12,6 @@ def train(batch_size=32, nb_type=3):
                 
     model.fit_generator(image_generactor.generator_4_multiple_types(batch_size=batch_size, nb_type=nb_type), 
                         samples_per_epoch=31745, nb_epoch=1,
-                        nb_worker=4,
+                        nb_worker=28,
                         validation_data=image_generactor.generator_4_multiple_types(batch_size=batch_size, nb_type=nb_type), nb_val_samples=1280)
     model.save("models/model_" + now + ".h5")
