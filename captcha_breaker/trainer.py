0c95c22c97cb09198ee9f60826c4d32124ac2384
DropletProbe <ys15162@my.bristol.ac.uk>
2018-06-27 16:08:03 +0800

    change iteration

--- captcha_breaker/trainer.py
+++ captcha_breaker/trainer.py
@@ -21,7 +21,7 @@ def continue_2_train(batch_size=32, nb_type=3):
     now = str(int(time.time()))
     model = load_model('models/model_1530071864.h5')
     model.fit_generator(image_generactor.generator_4_multiple_types(batch_size=batch_size, nb_type=nb_type), 
-                        samples_per_epoch=40860, nb_epoch=1,
+                        samples_per_epoch=4096, nb_epoch=1,
                         nb_worker=28,
                         validation_data=image_generactor.generator_4_multiple_types(batch_size=batch_size, nb_type=nb_type), nb_val_samples=1280)
     model.save("models/model_" + now + ".h5")
