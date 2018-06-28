4af341bfa429eda1609264d3703adb3dc0e2d6a2
DropletProbe <ys15162@my.bristol.ac.uk>
2018-06-28 12:57:42 +0800

    ..

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from captcha_breaker import trainer
 
-trainer.train(batch_size=128, nb_type=6)
+# trainer.train(batch_size=128, nb_type=6)
 # trainer.predict()
-# trainer.continue_2_train(batch_size=128, nb_type=4)
+trainer.continue_2_train(batch_size=128, nb_type=6)
 # trainer.test2()
 # trainer.test()
 # def move():
